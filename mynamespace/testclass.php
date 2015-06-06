6a74bffde719aaae9d61e944d63e82dd743d8bc9
wu <wu@we.de>
2015-06-06 10:29:50 +0200

    My new Commit

---
+++ mynamespace/testclass.php
@@ -0,0 +1,30 @@
+<?php
+namespace mynamespace;
+
+/**
+ *
+ * @author Damir
+ *        
+ */
+class testclass
+{
+    // TODO - Insert your code here
+    
+    /**
+     */
+    function __construct()
+    {
+        
+        // TODO - Insert your code here
+    }
+
+    /**
+     */
+    function __destruct()
+    {
+        
+        // TODO - Insert your code here
+    }
+}
+
+?>
